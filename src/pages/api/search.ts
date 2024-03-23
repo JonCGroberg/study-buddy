b83a118760fe2b5eedca5011b2d5055e75fa916e
Jonathan <jongroberg@gmail.com>
2024-03-23 18:07:33 -0400

    added stuff

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,14 +1,17 @@
 import type { APIRoute } from "astro";
 import { serverStudySearch } from "../../studybuddy";
 
+// {course:course_title}
 export const POST: APIRoute = async ({ request }) => {
-    if(request.headers.get("Content-Type") === "application/json") {
-        const body = await request.json();
-        if(body.course === undefined || typeof body.course !== "string")
-            return new Response(null, { status: 400 });
+	if (request.headers.get("Content-Type") === "application/json") {
+		const body = await request.json();
+		if (body.course === undefined || typeof body.course !== "string")
+			return new Response(null, { status: 400 });
 
-        const groups = await serverStudySearch((body.course as string).toUpperCase());
-        return new Response(JSON.stringify(groups), { status: 200 });
-    }
-    return new Response(null, { status: 400 });
+		const groups = await serverStudySearch(
+			(body.course as string).toUpperCase()
+		);
+		return new Response(JSON.stringify(groups), { status: 200 });
+	}
+	return new Response(null, { status: 400 });
 };
--- src/pages/components/Timeline.jsx
+++ src/pages/components/Timeline.jsx
@@ -6,7 +6,7 @@ const Timeline = ({ studyGroups }) => {
 				return (
 					<div className="">
 						<h6 className="w-100 border-1 border-bottom border-dark-subtle pb-2 mt-0 mb-4">
-							{new Date(timeSet[0]*1000).toString()}
+							{formatAMPM(new Date(timeSet[0] * 1000)).toString()}
 						</h6>
 						<div className="ps-5 ms-5 row gap-3">
 							{timeSet[1].map((group, index) => (
@@ -35,8 +35,8 @@ const Card = ({ cardData }) => {
 					</h3>{" "}
 					<div className="row col-sm m-0">
 						<h6 className="col-auto bg-secondary-subtle rounded-2 py-2">
-							Ends at {new Date(cardData.end*1000).toString()} & 1 hr 25 min
-							remaining
+							Ends at {formatAMPM(new Date(cardData.end * 1000)).toString()} & 1
+							hr 25 min remaining
 						</h6>
 						<h6 className="col-auto py-2">{cardData.location}</h6>
 					</div>
@@ -52,5 +52,30 @@ const Card = ({ cardData }) => {
 		</div>
 	);
 };
+
+function formatAMPM(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? "pm" : "am";
+	const weekDays = [
+		[
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday"
+		]
+	];
+	const day = weekDays[date.getDay()];
+	const today = weekDays[new Date(Date.now()).getDay()];
+
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+
+	return `${hours}:${minutes} ${ampm} ${day == today ? "Today" : day}`;
+}
 
 export default Timeline;
