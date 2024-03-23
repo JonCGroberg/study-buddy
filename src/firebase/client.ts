ac6d4f8e8c39bcb8f56be7fcef14fecf48e79030
Tigermouthbear <tigermouthbear@tigr.dev>
2024-03-23 11:44:22 -0400

    add basic firebase auth from astro docs

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhvh8TqeeU1crg753tdo7NCfQQRW8tlb8",
+  authDomain: "studdybuddy-e03b7.firebaseapp.com",
+  projectId: "studdybuddy-e03b7",
+  storageBucket: "studdybuddy-e03b7.appspot.com",
+  messagingSenderId: "567479764850",
+  appId: "1:567479764850:web:61141dacdfd5500a117feb",
+  measurementId: "G-C543SMWEY1"
+};
+
+export const app = initializeApp(firebaseConfig);
