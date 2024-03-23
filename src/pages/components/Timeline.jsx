73b25f47e881ee2dd9fa14ee6a6ba4d1ae412813
Jonathan <jongroberg@gmail.com>
2024-03-23 18:45:37 -0400

    fixed error for multiple elements in a timeslot

--- src/pages/components/Timeline.jsx
+++ src/pages/components/Timeline.jsx
@@ -1,7 +1,7 @@
 const Timeline = ({ studyGroups }) => {
 	console.log(studyGroups);
 	return (
-		<div className="p-5 row gap-3 mx-5">
+		<div className="py-5 px px-md-3 px-sm-5 row gap-3 mx-0 mx-md-3 mx-sm-5">
 			{studyGroups.map((timeSet) => {
 				return (
 					<div className="">
@@ -9,9 +9,10 @@ const Timeline = ({ studyGroups }) => {
 							{formatAMPM(new Date(timeSet[0] * 1000)).toString()}
 						</h6>
 						<div className="ps-5 ms-5 row gap-3">
-							{timeSet[1].map((group, index) => (
-								<Card key={index} cardData={group} />
-							))}
+							{timeSet[1].map((group, index) => {
+								console.log(group);
+								return <Card key={index} cardData={group} />;
+							})}
 						</div>
 					</div>
 				);
@@ -21,32 +22,33 @@ const Timeline = ({ studyGroups }) => {
 };
 
 const Card = ({ cardData }) => {
-	console.log(cardData);
+	// console.log(cardData);
 	return (
-		<div className="card">
+		<div className="card p-2">
 			<div className="card-body row">
 				<div className="col ">
 					{" "}
-					<h3 className="col">
+					<h4 className="col">
 						<span>
 							{cardData.course}
 							{cardData.name ? " - " + cardData.name : ""}
 						</span>
-					</h3>{" "}
+					</h4>{" "}
 					<div className="row col-sm m-0">
 						<h6 className="col-auto bg-secondary-subtle rounded-2 py-2">
-							Ends at {formatAMPM(new Date(cardData.end * 1000)).toString()} & 1
-							hr 25 min remaining
+							Ends {formatAMPM(new Date(cardData.end)).toString()}
 						</h6>
 						<h6 className="col-auto py-2">{cardData.location}</h6>
 					</div>
 				</div>
 
 				<div className="col-12 col-md-auto my-auto">
-					<button className="btn btn-primary p-2 mx-2">
-						Join This Group({cardData.buddies.length}/{cardData.max_buddies})
+					<button className="btn btn-outline-primary p-2 px-3 mx-2 me-4 fw-medium ">
+						Join This Group ({cardData.buddies.length}/{cardData.max_buddies})
 					</button>
-					<button className="btn p-2">More Details</button>
+					<button className="btn p-2 text-primary fw-medium ">
+						More Details
+					</button>
 				</div>
 			</div>
 		</div>
@@ -56,17 +58,15 @@ const Card = ({ cardData }) => {
 function formatAMPM(date) {
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
-	var ampm = hours >= 12 ? "pm" : "am";
+	var ampm = hours >= 12 ? "PM" : "AM";
 	const weekDays = [
-		[
-			"Sunday",
-			"Monday",
-			"Tuesday",
-			"Wednesday",
-			"Thursday",
-			"Friday",
-			"Saturday"
-		]
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
 	];
 	const day = weekDays[date.getDay()];
 	const today = weekDays[new Date(Date.now()).getDay()];
@@ -75,7 +75,7 @@ function formatAMPM(date) {
 	hours = hours ? hours : 12; // the hour '0' should be '12'
 	minutes = minutes < 10 ? "0" + minutes : minutes;
 
-	return `${hours}:${minutes} ${ampm} ${day == today ? "Today" : day}`;
+	return `${hours}:${minutes} ${ampm} ${day == today ? "" : day}`;
 }
 
 export default Timeline;
