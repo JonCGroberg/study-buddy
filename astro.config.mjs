b459628d63c33934a3e5ae7f2ba16fe8822a4c5a
Jonathan <jongroberg@gmail.com>
2024-03-24 16:24:14 -0400

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [react()],
-  adapter: netlify()
+  adapter: vercel()
 });
