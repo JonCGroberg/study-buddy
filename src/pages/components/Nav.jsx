3bcd54ce1eb4b92616f678574390d2a5e7706a63
Tigermouthbear <tigermouthbear@tigr.dev>
2024-03-24 01:08:28 -0400

    add signout to navbar

--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -3,7 +3,7 @@ import "../styles/styles.css"
 const Nav = ({ title = "", bookable = true }) => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-white shadow-sm border p-0 m-0">
-			<div className="container-fluid px-5">
+			<div className="container-fluid px-3">
 				<a className="navbar-brand" href="/">
 					<img src="/image.png"></img>
 					<span className=" px-3 text-primary  fw-bold    ">Study Buddy</span>
@@ -21,21 +21,13 @@ const Nav = ({ title = "", bookable = true }) => {
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav mb-2  mb-lg-0"></ul>
-					<form className="d-flex flex-grow-1 text-center  px-5 mx-5 " role="search">
+					<div className="d-flex flex-grow-1 text-center px-5 mx-5 ">
 						<h6 className="text-center w-100 fw-bold">
 							{title}
 						</h6>
-						{/* <input`
-							className="form-control me-2 "
-							type="search"
-							placeholder="Search"
-							aria-label="Search"
-						/>
-						<button className="btn btn-outline-dark " type="submit">
-							Search
-						</button> */}
-					</form>
-                    { bookable && (<a href="/book" className="btn btn-primary"> New Study Session</a>) }
+					</div>
+                    { bookable && (<a href="/book" className="btn btn-primary mx-2"> New Study Session</a>) }
+                    <a href="/api/auth/signout" className="btn btn-secondary mx-2">Signout</a>
 				</div>
 			</div>
 		</nav>
