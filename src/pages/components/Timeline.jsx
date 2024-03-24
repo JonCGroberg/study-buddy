2586cf681169d47464c2b07f115e8760d68e2067
Jonathan <jongroberg@gmail.com>
2024-03-23 21:44:15 -0400

    more aesthetic tweaks

--- src/pages/components/Timeline.jsx
+++ src/pages/components/Timeline.jsx
@@ -4,10 +4,10 @@ const Timeline = ({ studyGroups, userId }) => {
 		<div className="row gap-3 py-4 my-4 px-0 px-md-0 px-md-5 m-0 mx-lg-0 mx-md-5 ">
 			{studyGroups.map((sameHourGroups) => (
 				<div className="">
-					<h6 className="w-100 border-1 border-bottom border-dark-subtle pb-2 mt-0 mb-4">
+					<h6 className="small w-100 border-1 border-bottom border-dark-subtle pb-2 mt-0 mb-4">
 						{formatAMPM(new Date(sameHourGroups[0] * 1000)).toString()}
 					</h6>
-					<div className="px-3 ps-sm-3 ps-lg-5 ms-sm-3 ms-lg-5 row gap-3">
+					<div className="small px-3 ps-sm-3 ps-lg-5 ms-sm-3 ms-lg-5 row gap-3">
 						{sameHourGroups[1].map((group, index) => {
 							console.log(group);
 
@@ -38,17 +38,17 @@ const Card = ({ cardData, userId }) => {
 			<div className="card-body row">
 				<div className="col">
 					{" "}
-					<h4 className="col py-1">
-						<span>
+					<h6 className="col py-1">
+						<span className="">
 							{cardData.course}
 							{cardData.name ? " - " + cardData.name : ""}
 						</span>
-					</h4>{" "}
+					</h6>{" "}
 					<div className="row col-sm m-0">
-						<h6 className=" col-lg-auto col-12 bg-secondary-subtle rounded-2 py-2 ">
+						<h6 className="small col-lg-auto col-12 bg-secondary-subtle rounded-2 py-2 ">
 							Ends {formatAMPM(new Date(cardData.end)).toString()}
 						</h6>
-						<h6 className="col-auto py-2 small text-secondary ">
+						<h6 className=" col-auto py-2 small text-secondary ">
 							{cardData.location}
 						</h6>
 					</div>
@@ -56,16 +56,16 @@ const Card = ({ cardData, userId }) => {
 
 				<div className="col-12 col-md-auto my-auto">
 					{!userAlreadyJoined ? (
-						<button className="btn btn-outline-primary ms-0 p-2 px-3 mx-2 me-4 fw-medium ">
+						<button className="btn-sm btn btn-outline-primary ms-0 p-2 px-3 mx-2 me-4 fw-medium ">
 							{"Join This Group " + cardData.buddies.length}/
 							{cardData.max_buddies}
 						</button>
 					) : (
-						<button className="btn btn-outline-danger ms-0 p-2 px-3 mx-2 me-4 fw-medium ">
+						<button className="btn btn-sm btn-outline-danger ms-0 p-2 px-3 mx-2 me-4 fw-medium ">
 							{"Leave Group " + cardData.buddies.length}/{cardData.max_buddies}
 						</button>
 					)}
-					<button className="btn p-2 text-primary fw-medium ">
+					<button className="btn btn-sm p-2 text-primary fw-medium ">
 						More Details
 					</button>
 				</div>
