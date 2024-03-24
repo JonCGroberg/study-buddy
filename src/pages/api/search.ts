f2819e68c170d054c074b6588321476d0008855c
Tigermouthbear <tigermouthbear@tigr.dev>
2024-03-23 20:01:04 -0400

    ssr on course search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,15 +1,17 @@
 import type { APIRoute } from "astro";
-import { serverStudySearch } from "../../studybuddy";
+import { serverStudySearch, serverUserStudySearch } from "../../studybuddy";
 
 // {course:course_title}
 export const POST: APIRoute = async ({ request }) => {
     if(request.headers.get("Content-Type") === "application/json") {
         const body = await request.json();
-        if(body.course === undefined || typeof body.course !== "string")
-            return new Response(null, { status: 400 });
-
-        const groups = await serverStudySearch((body.course as string).toUpperCase());
-        return new Response(JSON.stringify(groups), { status: 200 });
+        if(body.course !== undefined && typeof body.course === "string") {
+            const groups = await serverStudySearch([(body.course as string).toUpperCase()]);
+            return new Response(JSON.stringify(groups), { status: 200 });
+        } else if(body.user !== undefined && typeof body.user === "string") {
+            const groups = await serverUserStudySearch(body.user);
+            return new Response(JSON.stringify(groups), { status: 200 });
+        }
     }
     return new Response(null, { status: 400 });
 };
--- src/studybuddy.ts
+++ src/studybuddy.ts
@@ -20,19 +20,29 @@ type StudyGroup = {
     max_buddies: number
 };
 
-async function serverStudySearch(course: string): Promise<StudyGroup[]> {
+async function serverStudySearch(courses: string[]): Promise<StudyGroup[]> {
     const snapshot = await db.collection("groups")
-                             .where("course", "==", course)
+                             .where("course", "in", courses)
                              .where("end", ">", Timestamp.now()).get();
     return Promise.all(snapshot.docs.map(async (doc: any): Promise<StudyGroup> => 
                 await createStudyGroupType(doc.data())));
+}
+
+async function serverUserStudySearch(user: string): Promise<StudyGroup[]> {
+    const owned = await db.collection("groups")
+                             .where("owner", "==", user)
+                             .where("end", ">", Timestamp.now()).get();
+    const joined = await db.collection("groups")
+                             .where("buddies", "array-contains-any", [user]).get();
+    return Promise.all([...owned.docs, ...joined.docs].map(
+        async (doc: any): Promise<StudyGroup> => await createStudyGroupType(doc.data())));
 }
 
 async function createStudyGroupType(doc: any): Promise<StudyGroup> {
     const owner = await getAuth(app).getUser(doc.owner);
     const buddies = await Promise.all(doc.buddies.map(async (buddy: string) => {
         const user = await getAuth(app).getUser(buddy);
-        return !user ? unknownUser : { name: user.displayName };
+        return !user ? unknownUser : { id: user.uid, name: user.displayName };
     }));
     return {
         course: doc.course,
@@ -72,4 +82,4 @@ async function serverStudyBook(booking: StudyBooking): Promise<WriteResult> {
     });
 }
 
-export { type User, type StudyGroup, serverStudySearch, serverStudyBook }
+export { type User, type StudyGroup, serverStudySearch, serverUserStudySearch, serverStudyBook }
