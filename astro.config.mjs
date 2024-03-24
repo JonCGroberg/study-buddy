23b3e23e5a82a3921407514ef5c5bddb722b69c4
Jonathan <jongroberg@gmail.com>
2024-03-23 22:10:11 -0400

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()]
+  integrations: [react()],
+  adapter: netlify()
 });
